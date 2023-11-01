bca81a49128233d6ff049e97e8c93c3e387ccf22
rongfengliang <1141591465@qq.com>
2023-11-01 09:59:53 +0800

    add more

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-// Imports are from the demo-util folder in the repo
-// https://github.com/torch2424/wasm-by-example/blob/master/demo-util/
-import { wasmBrowserInstantiate } from "./instantiateWasm.js";
 
-import wasmUrl from './main.wasm?url'
+import init from './main.wasm?init'
 
 const go = new Go(); // Defined in wasm_exec.js. Don't forget to add this in your index.html.
 
@@ -11,15 +8,16 @@ const runWasmAdd = async () => {
   const importObject = go.importObject;
 
   // Instantiate our wasm module
-  const wasmModule = await wasmBrowserInstantiate(wasmUrl, importObject);
+  const wasmModule = await init(importObject);
 
+  console.log(wasmModule)
   // Allow the wasm_exec go instance, bootstrap and execute our wasm module
-  go.run(wasmModule.instance);
+  go.run(wasmModule);
 
   // Call the Add function export from wasm, save the result
-  const addResult = wasmModule.instance.exports.add(2433, 24);
+  const addResult = wasmModule.exports.add(2433, 24);
 
-  const printName = wasmModule.instance.exports.printName();
+  const printName = wasmModule.exports.printName();
   // Set the result onto the body
   document.body.textContent = `Hello World! addResult: ${addResult} -- printName: ${printName}`;
 };
